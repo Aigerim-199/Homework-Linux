e7d56888fc24b73218de07f1659c1bf1389fa1b6
EC2 Default User <ec2-user@ip-172-31-92-146.ec2.internal>
2024-12-06 21:04:47 +0000

    folder

---
+++ Assignment9/age.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Please input your year of birth" year
+
+echo You are $((2024-$year)) years old in 2024.
+echo You were $((2020-$year)) years old in 2020.
+echo You will be $((2030-$year)) years old in 2030.
